2c60c35743a909189dec92a84a5fa0135e9962de
UpcraftLP <j-schirra@gmx.de>
2018-06-26 03:25:15 +0200

    added prefix logger

---
+++ src/api/java/com/github/upcraftlp/glasspane/api/package-info.java
@@ -0,0 +1,9 @@
+@MethodsReturnNonnullByDefault
+@ParametersAreNonnullByDefault
+@API(owner = "GlassPane", provides = "GlassPane|API", apiVersion = "0.1.0")
+package com.github.upcraftlp.glasspane.api;
+
+import mcp.MethodsReturnNonnullByDefault;
+import net.minecraftforge.fml.common.API;
+
+import javax.annotation.ParametersAreNonnullByDefault;
