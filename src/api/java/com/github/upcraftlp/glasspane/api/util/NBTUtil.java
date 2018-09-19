126e3534d3426c4498d4ca5ab502d1a9b70bdcb3
UpcraftLP <j-schirra@gmx.de>
2018-09-19 02:39:39 +0200

    more useful nbt utils

--- src/api/java/com/github/upcraftlp/glasspane/api/util/NBTUtil.java
+++ src/api/java/com/github/upcraftlp/glasspane/api/util/NBTUtil.java
@@ -6,11 +6,20 @@ import net.minecraft.nbt.NBTTagCompound;
 public class NBTUtil {
 
     public static NBTTagCompound getDefaultTagCompound(ItemStack stack) {
-        return getSubCompound(stack, "glasspane");
+        return getDefaultTagCompound(stack, true);
+    }
+
+    public static NBTTagCompound getDefaultTagCompound(ItemStack stack, boolean createIfAbsent) {
+        return getSubCompound(stack, "glasspane", createIfAbsent);
     }
 
     public static NBTTagCompound getSubCompound(ItemStack stack, String compoundName) {
-        return stack.getOrCreateSubCompound(compoundName);
+        return getSubCompound(stack, compoundName, true);
+    }
+
+    public static NBTTagCompound getSubCompound(ItemStack stack, String compoundName, boolean createIfAbsent) {
+        if(createIfAbsent) return stack.getOrCreateSubCompound(compoundName);
+        else return stack.hasTagCompound() ? stack.getTagCompound().getCompoundTag(compoundName) : new NBTTagCompound();
     }
 
     public static void setDefaultTag(ItemStack stack, NBTTagCompound nbt) {
@@ -20,4 +29,5 @@ public class NBTUtil {
     public static void setTagInfo(ItemStack stack, NBTTagCompound nbt, String compoundName) {
         stack.setTagInfo(compoundName, nbt);
     }
+
 }
