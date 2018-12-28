8c40a14aaae647560bd9bb546fb1853460491f41
UpcraftLP <j-schirra@gmx.de>
2018-12-28 15:26:34 +0100

    remove redundant api annotation

--- src/main/java/com/github/upcraftlp/glasspane/api/package-info.java
+++ src/main/java/com/github/upcraftlp/glasspane/api/package-info.java
@@ -1,9 +1,7 @@
 @MethodsReturnNonnullByDefault
 @ParametersAreNonnullByDefault
-@API(owner = "GlassPane", provides = "GlassPane|API", apiVersion = "0.2.1")
 package com.github.upcraftlp.glasspane.api;
 
 import mcp.MethodsReturnNonnullByDefault;
-import net.minecraftforge.fml.common.API;
 
 import javax.annotation.ParametersAreNonnullByDefault;
